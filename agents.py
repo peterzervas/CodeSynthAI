3e06e71f048bdd2a32270c9f5d2496ff9dc552d1
Peter <peter.zervas@nuago.com.au>
2024-04-05 22:09:10 +1030

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,7 +1,7 @@
 from textwrap import dedent
 from crewai import Agent
 from tools.AgentTools import AgentTools
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 
 class CustomAgents:
     def __init__(self, human_input=False, callbacks=None):
@@ -9,7 +9,6 @@ class CustomAgents:
         self.callbacks = callbacks
         self.OpenAIGPT35 = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
         self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4-0125-preview", temperature=0.4)
-        #self.Ollama = Ollama(model="openhermes") #Κλῦθί μευ, Ἑρμεία, Διὸς ἄγγελε, Μαιάδος υἱέ
 
     def technical_consultant(self):
         agent = Agent(
@@ -17,14 +16,17 @@ class CustomAgents:
             goal='Refine user requirements into clear and concise technical specifications',
             tools=AgentTools().tools(),
             backstory=dedent("""
-                As a Technical Consultant, your expertise lies in understanding user requirements and translating them into actionable technical specifications. Your insights will guide the development team to deliver a high-quality software solution that meets the user's needs.
+            As a Technical Consultant, your expertise lies in understanding user requirements and translating them into actionable technical specifications.
+            Your insights will guide the development team to deliver a high-quality software solution that meets the user's needs.
             """),
             verbose=True,
             memory=True,
             allow_delegation=True,
             callbacks=self.callbacks,
-            llm=self.OpenAIGPT4
+            llm=self.OpenAIGPT4,
+            function_calling_llm=self.OpenAIGPT4
         )
+        
         return agent
 
     def initial_coder(self):
@@ -33,14 +35,17 @@ class CustomAgents:
             goal='Generate clean, efficient, and well-documented code based on the technical specifications',
             tools=AgentTools().tools(),
             backstory=dedent("""
-                As an Initial Coder, your responsibility is to transform the technical specifications into functional and maintainable code. Your coding skills and attention to detail will lay the foundation for a robust software solution.
+            As an Initial Coder, your responsibility is to transform the technical specifications into functional and maintainable code.
+            Your coding skills and attention to detail will lay the foundation for a robust software solution.
             """),
             verbose=True,
             memory=True,
             allow_delegation=True,
             callbacks=self.callbacks,
-            llm=self.OpenAIGPT4
+            llm=self.OpenAIGPT4,
+            function_calling_llm=self.OpenAIGPT4
         )
+        
         return agent
 
     def senior_code_reviewer(self):
@@ -49,14 +54,17 @@ class CustomAgents:
             goal='Review the generated code and provide feedback for improvement',
             tools=AgentTools().tools(),
             backstory=dedent("""
-                As a Senior Code Reviewer, your task is to ensure the quality and integrity of the generated code. Your expertise in identifying potential issues, suggesting optimizations, and enforcing coding best practices will elevate the code to the highest standards.
+            As a Senior Code Reviewer, your task is to ensure the quality and integrity of the generated code.
+            Your expertise in identifying potential issues, suggesting optimizations, and enforcing coding best practices will elevate the code to the highest standards.
             """),
             verbose=True,
             memory=True,
             allow_delegation=True,
             callbacks=self.callbacks,
-            llm=self.OpenAIGPT4
+            llm=self.OpenAIGPT4,
+            function_calling_llm=self.OpenAIGPT4
         )
+        
         return agent
 
     def tester(self):
@@ -65,12 +73,16 @@ class CustomAgents:
             goal='Develop comprehensive test cases and execute them against the generated code',
             tools=AgentTools().tools(),
             backstory=dedent("""
-                As a Tester, your mission is to thoroughly validate the functionality and reliability of the generated code. Your meticulous testing approach and attention to edge cases will ensure a robust and error-free software solution.
-            once testing is completed, provide the functional code in full, not the test code"""),
+            As a Tester, your mission is to thoroughly validate the functionality and reliability of the generated code.
+            Your meticulous testing approach and attention to edge cases will ensure a robust and error-free software solution.
+            Once testing is completed, provide the functional code in full, not the test code.
+            """),
             verbose=True,
             memory=True,
             allow_delegation=True,
             callbacks=self.callbacks,
-            llm=self.OpenAIGPT4
+            llm=self.OpenAIGPT4,
+            function_calling_llm=self.OpenAIGPT4
         )
+        
         return agent
